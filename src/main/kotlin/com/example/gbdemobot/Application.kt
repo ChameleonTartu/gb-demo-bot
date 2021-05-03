ba2cce764a3cfd4627bba16eddb2c01c46cb6cb6
Dmytro Chasovskyi <dmytro.chasovskyi@greenbird.com>
2021-05-03 17:24:59 +0200

    Make loop, so bot is running constantly in Docker file

--- src/main/kotlin/com/example/gbdemobot/Application.kt
+++ src/main/kotlin/com/example/gbdemobot/Application.kt
@@ -7,26 +7,31 @@ import org.slf4j.LoggerFactory
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
+import java.lang.Exception
 
 
 val log: Logger = LoggerFactory.getLogger("main")
 
 fun main(args: Array<String>) {
-    val token = args[0]
-    val telegramBotsApi = TelegramBotsApi(DefaultBotSession::class.java).registerBot(
-        GbBot(
-            gbProcessor = GbProcessor(),
-            token = token
+    while (true) {
+        val token = args[0]
+        val telegramBotsApi = TelegramBotsApi(DefaultBotSession::class.java).registerBot(
+            GbBot(
+                gbProcessor = GbProcessor(),
+                token = token
+            )
         )
-    )
-    telegramBotsApi.stop()
-    try {
-        log.info("Starting the bot ...")
-        telegramBotsApi.start()
-    } catch (e: TelegramApiException) {
-        log.error("Telegram API failure", e)
-    } finally {
-        log.info("Stopping the bot ...")
         telegramBotsApi.stop()
+        try {
+            log.info("Starting the bot ...")
+            telegramBotsApi.start()
+        } catch (e: TelegramApiException) {
+            log.error("Telegram API failure", e)
+        } catch (e: Exception) {
+            log.error("Telegram API unexpected", e)
+        } finally {
+            log.info("Stopping the bot ...")
+            telegramBotsApi.stop()
+        }
     }
 }
